6efbfc9c0ed2a179289b021d9783d01dff4fc0f4
Ciro Santilli <ciro.santilli@gmail.com>
2019-02-12 11:39:56 +0000

    getopt: better example

--- getopt_example.sh
+++ getopt_example.sh
@@ -1,53 +1,59 @@
 #!/usr/bin/env bash
 set -eu
 
-# CLI arguments.
-parsed=$(getopt -o dfo:v -l debug,force,output:,verbose -- "$@")
-echo "parsed: ${parsed}"
-eval set -- "$parsed"
-d=n
-f=n
-v=n
+
+# Default values.
+debug=false
+force=false
+noshort=false
 out_path=-
+verbose=false
+
+# Handle CLI arguments.
+parsed=$(getopt -o d,f,,o:,v -l debug,force,noshort,output:,verbose -- "$@")
+eval set -- "$parsed"
 while true; do
   case "$1" in
     -d|--debug)
-      d=y
+      debug=true
       shift
       ;;
     -f|--force)
-      f=y
+      force=true
       shift
       ;;
-    -v|--verbose)
-      v=y
+    --noshort)
+      noshort=true
       shift
       ;;
     -o|--output)
       out_path="$2"
       shift 2
       ;;
+    -v|--verbose)
+      verbose=true
+      shift
+      ;;
     --)
       shift
       break
-      ;;
-    *)
-      echo "Programming error"
-      exit 3
       ;;
   esac
 done
 if [ $# -gt 0 ]; then
   in_path="$1"
+  shift
 else
   in_path=-
 fi
 
 # Action.
 printf "\
-verbose: ${v}
-force: ${f}
-debug: ${d}
-in: ${in_path}
-out: ${out_path}
+verbose: ${verbose}
+force: ${force}
+debug: ${debug}
+in_path: ${in_path}
+noshort: ${noshort}
+out_path: ${out_path}
+positional: $@
 "
